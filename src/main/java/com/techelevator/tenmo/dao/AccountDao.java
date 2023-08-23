2fc92b909788aa3e4e4942eb5933af1fdf831f0f
James Lavin <jblavin12@gmail.com>
2023-08-23 16:48:59 -0400

    changes to Account model and accountDao

--- src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,4 +1,23 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public interface AccountDao {
+    <List> Account list();
+
+    Account getAccount(int accountId);
+
+    Account createAccount(Account account);
+
+    void updateBalance(int accountId, BigDecimal updatedBalance);
+
+    Account deleteAccountById (int accountId);
+
+
+
+
 }
