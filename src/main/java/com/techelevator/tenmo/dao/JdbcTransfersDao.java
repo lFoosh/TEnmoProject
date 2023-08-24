74185f0de471f9bb8bb9022caf853836c763395b
Michael <blissfulpandagaming@gmail.com>
2023-08-24 15:32:39 -0400

    stuff we did 8/24

--- src/main/java/com/techelevator/tenmo/dao/JdbcTransfersDao.java
+++ src/main/java/com/techelevator/tenmo/dao/JdbcTransfersDao.java
@@ -1,4 +1,80 @@
 package com.techelevator.tenmo.dao;
 
-public class JdbcTransfersDao {
+import com.techelevator.tenmo.model.Transfers;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class JdbcTransfersDao implements TransfersDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTransfersDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Transfers> findAllTransfersForUser(int userId) {
+        String sql = "SELECT * FROM transfers WHERE userId = ?";
+        return jdbcTemplate.query(sql, new TransferRowMapper(), userId);
+    }
+
+    @Override
+    public Transfers createTransfer(Transfers transfers) {
+        return null;
+    }
+
+    @Override
+    public Transfers getTransferById(int transferId) {
+        return null;
+    }
+
+    @Override
+    public int findIdBy(String username) {
+        return 0;
+    }
+    /*
+     * This method manages the transaction for transferring funds between accounts.
+     * The @Transactional annotation ensures that if any part of the transaction fails
+     * (for instance, money gets deducted from the sender but fails to add to the receiver),
+     * then the entire operation is rolled back to maintain data integrity.
+     */
+   @Transactional
+    public void transfer(int senderId, int receiverId,BigDecimal transferAmount){
+        BigDecimal sendingAccountBalance = getCurrentBalance(senderId);
+        if(transferAmount.compareTo(BigDecimal.ZERO) <= 0 || sendingAccountBalance.compareTo(transferAmount) < 0) {
+            throw new IllegalArgumentException("Insufficient Funds.");
+        }
+            updateTransferBalance(senderId, transferAmount.negate());
+            updateTransferBalance(receiverId, transferAmount);
+        }
+
+    public void updateTransferBalance(int userId, BigDecimal amount){
+        String sql = "UPDATE account SET balance = balance + ? WHERE user_id = ?";
+        jdbcTemplate.update(sql, amount, userId);
+    }
+
+    //current balance of a user
+    public BigDecimal getCurrentBalance(int userId) {
+        String sql = "SELECT balance FROM account WHERE user_id = ?";
+        return jdbcTemplate.queryForObject(sql, BigDecimal.class, userId);
+    }
+
+    private class TransferRowMapper implements RowMapper<Transfers> {
+        @Override
+        public Transfers mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Transfers transfer = new Transfers();
+            transfer.setTransferId(rs.getInt("transfer_id"));
+            transfer.setTransferAmount(rs.getBigDecimal("amount"));
+            transfer.setSenderId(rs.getInt("sender_id"));
+            transfer.setReceiverId(rs.getInt("receiver_id"));
+            transfer.setTransferStatus(rs.getString("transfer_status"));
+            return transfer;
+        }
+    }
 }
--- src/main/java/com/techelevator/tenmo/dao/TransfersDao.java
+++ src/main/java/com/techelevator/tenmo/dao/TransfersDao.java
@@ -1,4 +1,47 @@
 package com.techelevator.tenmo.dao;
 
+
+import com.techelevator.tenmo.model.Transfers;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface TransfersDao {
-}
+
+        /**
+         * Retrieves all transfers associated with a specific user.
+         *
+         * @param userId ID of the user.
+         * @return List of Transfers.
+         */
+        List<Transfers> findAllTransfersForUser(int userId);
+
+        /**
+         * Creates a new transfer record in the system.
+         *
+         * @param transfers The transfer details.
+         * @return The created transfer.
+         */
+        Transfers createTransfer(Transfers transfers);
+
+        /**
+         * Retrieves the details of a specific transfer using its ID.
+         *
+         * @param transferId ID of the transfer.
+         * @return Transfer details.
+         */
+        Transfers getTransferById(int transferId);
+
+        /**
+         * Retrieves the user ID using a username.
+         *
+         * @param username Name of the user.
+         * @return ID of the user.
+         */
+        int findIdBy(String username);
+
+        void transfer(int senderId, int receiver, BigDecimal transferAmount);
+        void updateTransferBalance(int userId, BigDecimal amount);
+        BigDecimal getCurrentBalance(int userId);
+    }
+
