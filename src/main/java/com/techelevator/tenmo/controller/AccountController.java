34b1dfd1c530a0a23e9cd0822810c51322ca96c3
James Lavin <jblavin12@gmail.com>
2023-08-24 20:45:13 -0400

    changes to AccountController

--- src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,30 +1,41 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Account;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
+import java.security.Principal;
 
 @RestController
+@RequestMapping("/api")
 public class AccountController {
 
-    private AccountDao accountDao;
+    private final AccountDao accountDao;
+    private final UserDao userDao;  // Add this to get userId by username
 
-    public AccountController(AccountDao accountDao){
+    public AccountController(AccountDao accountDao, UserDao userDao) {
         this.accountDao = accountDao;
+        this.userDao = userDao;   // Initialize the userDao
     }
 
-    @RequestMapping(path = "api/balance", method = RequestMethod.GET)
-//    @PreAuthorize("isAuthenticated()")
-    public BigDecimal getBalanceByUserID(Integer userId) {
-        return accountDao.getBalanceByUserID(userId);
-    }
+    @GetMapping("/balance")
+    @PreAuthorize("isAuthenticated()")
+    public String getBalanceByUsername(Principal principal) {
+        String currentUsername = principal.getName(); // Fetch the balance for the userId
+        int userId = userDao.findIdByUsername(currentUsername);  // Fetch the userId based on username
 
+        BigDecimal balance = accountDao.getBalanceByUserID(userId);
+        int intBalance = balance.intValue();
+
+        return "{ \n" +
+                "username: " + principal.getName() + "\n" +
+                "balance: " + intBalance + "\n }";
+    }
 }
