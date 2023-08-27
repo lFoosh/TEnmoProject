4fee9535bb4988bee50cd4c8938816339df3f0e9
James Lavin <jblavin12@gmail.com>
2023-08-26 21:30:21 -0400

    cleaning up excess methods in project

--- src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -7,16 +7,20 @@ import java.util.List;
 
 public interface AccountDao {
 
-    List<Account> list();
-
-    Account getAccount(int accountId);
-
+    /**
+     * Retrieves the balance of an account associated with a specific user ID.
+     *
+     * @param id The user ID for which the balance needs to be retrieved.
+     * @return The account balance as a BigDecimal value.
+     */
     BigDecimal getBalanceByUserID(int id);
 
-    void updateBalance(int accountId, BigDecimal updatedBalance);
-
-    Account deleteAccountById (int accountId);
-
+    /**
+     * Fetches an Account object based on a user ID.
+     *
+     * @param userId The user ID for which the account needs to be fetched.
+     * @return The Account object containing account details.
+     */
     Account getByUserId (int userId);
 
 
